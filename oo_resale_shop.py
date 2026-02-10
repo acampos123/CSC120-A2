cda6fa65977380e29431e98f03722dcb2b5f4425
acampos123 <acampos@smith.edu>
2026-02-09 22:32:19 -0500

    Added multiple functions

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,24 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
+    inventory = []
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory: list):
+     self.inventory = inventory
 
     # What methods will you need?
+    def buy(self):
+       self.inventory.append(Computer)
+        
+    def sell(self):
+       if Computer in inventory:
+        self.inventory.remove(computer)
+        else: 
+        return("Computer not found. Please select another item to sell.")
+        
+   
+        
